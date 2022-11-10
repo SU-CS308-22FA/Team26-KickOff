9993c8495c610a33482622573035c370cac944c1
sermetozgu <sermetozgu@sabanciuniv.edu>
2022-11-10 21:59:34 +0300

    heroku changes

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ app.use((err, req, res, next) => {
   console.log(err);
   next();
 });
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
