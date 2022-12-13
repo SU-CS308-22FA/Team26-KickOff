24e094cd8de2a22bd24c08744bc5203d37cbf2e6
bturann <berkaybaristuran@gmail.com>
2022-12-13 22:28:36 +0300

    Add contact information

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import "../styles/home.css"
 
 const Homepage = () => {
 
     return (
         <>
             <h1>Welcome to Homepage which is only visible when you are logged in </h1>
+            <div class="sub_div">
+                <p>For any questions and platform related problems please contact: bturan@sabanciuniv.edu</p>
+            </div>
         </>
     )
 }
